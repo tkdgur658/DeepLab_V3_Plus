bb5a2ac9646c8899dadf9b4bbe3d720baa847906
tkdgur658 <59380289+tkdgur658@users.noreply.github.com>
2024-10-07 12:54:17 +0900

    Update DeepLab_V3_Plus.py

--- DeepLab_V3_Plus.py
+++ DeepLab_V3_Plus.py
@@ -51,7 +51,7 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, in_channels, block, layers, output_stride, BatchNorm, pretrained=True):
+    def __init__(self, in_channels, block, layers, output_stride, BatchNorm, pretrained=False):
         self.inplanes = 64
         super(ResNet, self).__init__()
         blocks = [1, 2, 4]
